68af706f2f40597e79a8e8802c288c7736c0d64e
轶桐 吴 <3420675312@qq.com>
2023-03-20 13:10:58 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathode_aufgabe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double sub;
+            double sub2;
+            double su;
+            string antwort;
+            
+            Console.WriteLine("geben sie zwei zahlen ein:");
+            Console.Write("a=");
+            sub = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            sub2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("rechnung (+,-,+,/)");
+            antwort = Console.ReadLine();
+            if (antwort == "+")
+            {
+             Console.WriteLine("addition");
+            su = addition(sub, sub2);
+            Console.WriteLine("ergabnes des addition : " + su);
+            }
+            else if (antwort == "-")
+            {
+               Console.WriteLine("subtraktion");
+            su = subtraktion(sub, sub2);
+            Console.WriteLine("ergabnes des subtraktion : " + su);
+            }
+            else if (antwort == "*")
+            {
+             Console.WriteLine("multtiplikation");
+            su = multiblikation(sub, sub2);
+            Console.WriteLine("ergabnes des multiblikation : " + su);
+            }
+            else if (antwort == "/")
+            {
+
+            Console.WriteLine("divison");
+            su = division(sub, sub2);
+            Console.WriteLine("ergabnes des division : " + su);
+            }
+            else
+            {
+                Console.WriteLine("falsch eingewält");
+            }
+                     
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        static double subtraktion(double n, double m)
+        {
+            return n - m;
+        }
+        static double multiblikation(double a ,double b)
+        {
+            return a * b;   
+        }
+        static double addition(double c , double d)
+        {
+            return c + d;
+        }
+        static double division(double v, double n)
+        {
+            return v % n;
+        }
+
+        
+        
+
+    }
+}
